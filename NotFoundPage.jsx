e18c075740ba2907ab009edb2ab9dc4d98e398a6
Nandhni03 <nandhnigabriela@yahoo.com>
2024-05-29 19:19:46 +0300

    New beginning

--- NotFoundPage.jsx
+++
@@ -1,8 +0,0 @@
-import { Link } from 'react-router-dom'
-
-export default function NotFoundPage() {
-    return <div className='flex flex-col gap-2'>
-        <h1>404 Not Found</h1>
-        <h2><Link to='/'>Home Table</Link></h2>
-    </div>
-}
